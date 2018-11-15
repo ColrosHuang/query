bd41c80a76ac9efb04be909c519ca2911faee715
Kevin <6486589292qq.com>
2018-11-15 21:26:36 +0800

    add navbar

--- src/main/java/com/asiainfo/Controller/QueryIDTypeController.java
+++ src/main/java/com/asiainfo/Controller/QueryIDTypeController.java
@@ -1,17 +1,9 @@
 package com.asiainfo.Controller;
 
 import com.asiainfo.Util.NatsClient;
-import io.nats.client.Dispatcher;
-import io.nats.client.Message;
-import io.nats.client.Subscription;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class QueryIDTypeController {
@@ -21,26 +13,15 @@ public class QueryIDTypeController {
             @PathVariable("queryId") String queryId
     ) {
         //System.out.println(queryId);
-        try {
-            NatsClient client = new NatsClient();
-            Subscription subscription1 = client.getConnection().subscribe("hello");
-            Subscription subscription2 = client.getConnection().subscribe("hello1");
-            Subscription subscription3 = client.getConnection().subscribe("hello2");
+//		NatsClient client = new NatsClient();
 
-            client.send("QueryDaemonForJava","hello", queryId);
-            String msg1 = new String(subscription1.nextMessage(Duration.ofMillis(5000)).getData());
-            String msg2 = new String(subscription2.nextMessage(Duration.ofMillis(5000)).getData());
-            String msg3 = new String(subscription3.nextMessage(Duration.ofMillis(5000)).getData());
+        //发送数据并接收一条返回
+        //String msg = client.request("QueryDaemonForJava", queryId, queryId, Duration.ofSeconds(5));
+        String msg = NatsClient.nats_request("QueryDaemonForJava", queryId, queryId);
+        if (msg == null) {
+            return "hi no reply";
+        } else
+            return "hi: " + msg;
 
-            return "hi: " + msg1 + msg2 + msg3;
-            subscription1.unsubscribe();
-
-
-        }catch (Exception e){
-            System.out.println(e.getStackTrace());
-        }
-
-
-        return "hi no reply";
     }
 }
